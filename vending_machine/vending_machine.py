19cc4b32a1ed56416443c4e93abcb017035b970a
Hiromu Shioya <kwappa.856@gmail.com>
2024-06-11 14:09:44 +0900

    :green_circle: Implement to sum deposit

--- vending_machine/vending_machine.py
+++ vending_machine/vending_machine.py
@@ -1,9 +1,11 @@
 class VendingMachine:
     def __init__(self):
+        self._deposit = 0
         pass
 
     def insert_money(self, money):
+        self._deposit += money
         return money
 
     def deposit(self):
-        return 150
+        return self._deposit
